c3aabf51067af5b1415a122f27c52654b2d735f3
Mohamed Asif <mohamedasif18@gmail.com>
2021-03-27 18:28:24 +0530

    Added core functionality

--- mat-dynamic-form/src/app/components/provider-criteria-assignment/provider-criteria-assignment.component.ts
+++ mat-dynamic-form/src/app/components/provider-criteria-assignment/provider-criteria-assignment.component.ts
@@ -4,9 +4,10 @@ import { map, startWith } from 'rxjs/operators'
 import { FormGroup, FormBuilder, Validators, FormControl, FormArray, FormArrayName } from '@angular/forms';
 import { DataService } from '../../services';
 
-interface FormControlOptions {
+interface FormDependencyData {
   bsOptions?: Array<string>;
   bsFilteredOptions?: Observable<string[]>;
+  providerAssignmentActions?: Array<any>;
 }
 @Component({
   selector: 'app-provider-criteria-assignment',
@@ -17,9 +18,14 @@ export class ProviderCriteriaAssignmentComponent implements OnInit {
 
   public filteredOptions: Observable<string[]>;
   public blueShieldInfoForm: FormGroup;
-  public formControlOptions: FormControlOptions = {
+  public formDependencyData: FormDependencyData = {
     bsOptions: ['One', 'Two', 'Three'],
     bsFilteredOptions: new Observable(),
+    providerAssignmentActions: [
+      { label: 'BAU', value: 'BAU', checked: false, disabled: false },
+      { label: 'Reject', value: 'Reject', checked: false, disabled: false },
+      { label: 'Pay', value: 'Pay', checked: false, disabled: false }
+    ]
   };
 
   constructor(private formBuilder: FormBuilder, private dataService: DataService) { }
@@ -34,13 +40,13 @@ export class ProviderCriteriaAssignmentComponent implements OnInit {
   }
 
   private generateBsFiteredOptions() {
-    this.formControlOptions.bsFilteredOptions = this.blueShieldInfoForm.controls['bsNumber'].valueChanges
+    this.formDependencyData.bsFilteredOptions = this.blueShieldInfoForm.controls['bsNumber'].valueChanges
       .pipe(startWith(''), map(value => this.bsFiter(value)));
   }
 
   private bsFiter(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.formControlOptions.bsOptions.filter(option => option.toLowerCase().includes(filterValue));
+    return this.formDependencyData.bsOptions.filter(option => option.toLowerCase().includes(filterValue));
   }
 
   public bsNumberChange(event: any): void {
@@ -68,13 +74,64 @@ export class ProviderCriteriaAssignmentComponent implements OnInit {
   private generateAdditionalCriteria(): FormGroup {
     return this.formBuilder.group({
       criteriaName: ['', Validators.required],
-      criteriaDetails: this.formBuilder.array([])
+      criteriaDetails: this.formBuilder.array([this.generateCriteriaDetails()])
     });
-
   }
 
   get additionalCriterias(): FormArray {
     return this.blueShieldInfoForm.get('criterias') as FormArray;
   }
 
+  public removeAdditionalCriteria(i: number): void {
+    this.additionalCriterias.removeAt(i);
+  }
+
+  public addAdditionalDates(criteria: FormGroup): void {
+    this.additionalCriteriaDetails(criteria).push(this.generateCriteriaDetails());
+  }
+
+  public removeAdditionalDates(criteria: FormGroup, j: number): void {
+    this.additionalCriteriaDetails(criteria).removeAt(j);
+  }
+
+  public addAdditionalMemberType(criteriaDetail: FormGroup): void {
+    this.additionalMemberDetails(criteriaDetail).push(this.generateMemberDetails());
+  }
+
+  public generateCriteriaDetails(): FormGroup {
+    return this.formBuilder.group({
+      criteriaStartDate: ['', Validators.required],
+      criteriaEndDate: ['', Validators.required],
+      memberDetails: this.formBuilder.array([this.generateMemberDetails()])
+    });
+  }
+
+  public generateMemberDetails(): FormGroup {
+    return this.formBuilder.group({
+      memberType: ['', Validators.required],
+      assignmentAction: ['', Validators.required],
+      rate: ['', Validators.required],
+      overlap: ['', Validators.required],
+      paymentExceptions: ['', Validators.required],
+      field1: ['', Validators.required],
+      field2: ['', Validators.required],
+      field3: ['', Validators.required],
+    });
+  }
+
+  public additionalCriteriaDetails(criteria: FormGroup): FormArray {
+    return criteria.get('criteriaDetails') as FormArray;
+  }
+
+  public additionalMemberDetails(criteriaDetail: FormGroup): FormArray {
+    return criteriaDetail.get('memberDetails') as FormArray;
+  }
+
+  public removeMemberType(criteriaDetail: FormGroup, k: number) {
+    this.additionalMemberDetails(criteriaDetail).removeAt(k);
+  }
+
+  public getFormGroupValue(formGroup: FormGroup, formControlName: string) {
+    return formGroup.controls[formControlName].value;
+  }
 }
